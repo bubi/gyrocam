4cad7ba4188ba7c7248bb238a9ab8d1e304847bf
bubi <m.bubestinger@gmail.com>
2013-01-24 17:06:44 +0100

    added LED for system load check and changed output to accel angles for debugging

--- config/target_config.h
+++ config/target_config.h
@@ -2,8 +2,8 @@
 
 #define LED_PORT 0		// Port for led
 #define LED_BIT 7		// Bit on port for led
-#define LED_ON 1		// Level to set port to turn on led
-#define LED_OFF 0		// Level to set port to turn off led
+#define LED_ON  LED_PORT,LED_BIT,1		// Level to set port to turn on led
+#define LED_OFF LED_PORT,LED_BIT,0		// Level to set port to turn off led
 
 #define MPU6050_ADRESS	0x6B
 #define UART_BAUD 		115200
--- src/gyro_main.c
+++ src/gyro_main.c
@@ -35,13 +35,15 @@ int main (void){
 	float roll_angle;
 	uint8_t angle[10];
 	uint8_t crlf[] = { 0x0D, 0x0A }; // \n\r
+	uint8_t clearscreen[] = {0x0C};
 
 	/* Init Systick to 1ms */
-	SysTick_Config(0xFFFF); /* TBD */
+	SysTick_Config(SystemCoreClock / 100); /* TBD */
 	/* Initialize GPIO (sets up clock) */
 	GPIOInit();
 	/* Set LED port pin to output */
 	GPIOSetDir( LED_PORT, LED_BIT, 1 );
+	GPIOSetValue( LED_OFF );
 
 	UARTInit(UART_BAUD); // 115200;
 
@@ -56,20 +58,23 @@ int main (void){
 
 	if(MPU6050_whoami()){
 		/*error*/
-		return 1;
 	}
 
 	/*kalman form http://tom.pycke.be/ */
 	init_Gyro1DKalman(&filter_roll, 0.0001, 0.0003, 0.69);
 
 	while(1){
-
+		GPIOSetValue( LED_ON );
 		dt = get_Time_s();
-		ars_predict(&filter_roll, MPU6050_getGyroRoll_rad() , dt);  // Kalman predict
+		//ars_predict(&filter_roll, MPU6050_getGyroRoll_rad() , dt);  // Kalman predict
 		acc_tmp = -(atan2(-MPU6050_getAccel_z(), MPU6050_getAccel_y())-(3.14159/2.0)); /* calculate accel angle */
-	    roll_angle = ars_update(&filter_roll, acc_tmp);        // Kalman update + result (angle)
-	    memcpy(angle, &roll_angle, sizeof(roll_angle));		   // convert to char array
-	    UARTSend( (uint8_t *) angle, 10);					   // print
+	    //roll_angle = ars_update(&filter_roll, acc_tmp);        // Kalman update + result (angle)
+
+	    memcpy(angle, &acc_tmp, sizeof(roll_angle));		   // convert to char array
+
+		UARTSend ((uint8_t *) clearscreen,1);
+		UARTSend ((uint8_t *) angle,10);
 	    UARTSend( (uint8_t *) crlf, 2);						   // \n \r
+	    GPIOSetValue( LED_OFF );
 	}
 }
