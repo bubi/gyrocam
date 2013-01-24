d81d1df9d5d438c02c69de8d4934d25b49410f08
unknown <bumi@PC-BUMI1.quipos-solutions.local>
2013-01-24 15:29:35 +0100

    init branch

---
+++ src/drv/mpu6050.c
@@ -0,0 +1,153 @@
+/*
+ * mpu6050.c
+ *
+ *  Created on: Jan 24, 2013
+ *      Author: bumi
+ */
+
+#include "mpu6050.h"
+#include "target_config.h"
+#include "driver_config.h"
+
+#include "i2c.h"
+
+extern volatile uint8_t 	I2CMasterBuffer[BUFSIZE], I2CSlaveBuffer[BUFSIZE];
+extern volatile uint32_t 	I2CReadLength, I2CWriteLength;
+
+
+uint8_t MPU6050_init() {
+
+	uint8_t state;
+
+	I2CWriteLength 		= 3;
+	I2CReadLength		= 0;
+	I2CMasterBuffer[0] 	= MPU6050_ADRESS;
+	I2CMasterBuffer[1] 	= MPU6050_RA_PWR_MGMT_1;
+	I2CMasterBuffer[2] 	= 0b10000000; // reset device;
+
+	state = I2CEngine();
+	if(state != I2C_OK) return 1;
+
+	I2CWriteLength 		= 3;
+	I2CReadLength		= 0;
+	I2CMasterBuffer[0] 	= MPU6050_ADRESS;
+	I2CMasterBuffer[1] 	= MPU6050_RA_PWR_MGMT_1;
+	I2CMasterBuffer[2] 	= 0b00000011; // wakeup and set clock to z-axis gyro;
+
+	state = I2CEngine();
+	if(state != I2C_OK) return 1;
+
+
+	I2CWriteLength 		= 3;
+	I2CReadLength		= 0;
+	I2CMasterBuffer[0] 	= MPU6050_ADRESS;
+	I2CMasterBuffer[1] 	= MPU6050_RA_GYRO_CONFIG;
+	I2CMasterBuffer[2] 	= 0b00000000; // gyro range defaults to 250°/s but for future use
+
+	state = I2CEngine();
+	if(state != I2C_OK) return 1;
+
+	I2CWriteLength 		= 3;
+	I2CReadLength		= 0;
+	I2CMasterBuffer[0] 	= MPU6050_ADRESS;
+	I2CMasterBuffer[1] 	= MPU6050_RA_ACCEL_CONFIG;
+	I2CMasterBuffer[2] 	= 0b00000001; // 4G range
+
+	state = I2CEngine();
+	if(state != I2C_OK) return 1;
+
+	return 0;
+}
+
+uint8_t MPU6050_whoami(){
+
+	uint8_t state;
+
+	I2CWriteLength 		= 1;
+	I2CReadLength		= 1;
+	I2CMasterBuffer[0] 	= MPU6050_ADRESS;
+	I2CMasterBuffer[1] 	= MPU6050_RA_WHO_AM_I;
+	I2CMasterBuffer[2] 	= MPU6050_ADRESS | RD_BIT;
+
+	state = I2CEngine();
+	if(state != I2C_OK) return 1;
+
+	if(I2CSlaveBuffer[0] != 0x34) return 1;
+
+	return 0;
+}
+
+uint8_t MPU6050_getValue(sMPU_Value* sValue){
+
+	uint8_t state;
+
+	I2CWriteLength 		= 1;
+	I2CReadLength		= 14;
+	I2CMasterBuffer[0] 	= MPU6050_ADRESS;
+	I2CMasterBuffer[1] 	= MPU6050_RA_ACCEL_XOUT_H;
+	I2CMasterBuffer[2] 	= MPU6050_ADRESS | RD_BIT;
+
+	state = I2CEngine();
+	if(state != I2C_OK) return 1;
+
+	sValue = I2CSlaveBuffer;
+
+	return 0;
+}
+
+float MPU6050_getGyroRoll_rad(){
+
+	uint16_t tmp;
+	uint8_t state;
+
+	I2CWriteLength 		= 1;
+	I2CReadLength		= 2;
+	I2CMasterBuffer[0] 	= MPU6050_ADRESS;
+	I2CMasterBuffer[1] 	= MPU6050_RA_GYRO_XOUT_H;
+	I2CMasterBuffer[2] 	= MPU6050_ADRESS | RD_BIT;
+
+	state = I2CEngine();
+	if(state != I2C_OK) return 1;
+
+	tmp = (I2CSlaveBuffer[0] << 8) | I2CSlaveBuffer[1] ;
+
+	return tmp * 123; /* TBD */
+}
+
+float MPU6050_getAccel_y(){
+
+	uint16_t tmp;
+	uint8_t state;
+
+	I2CWriteLength 		= 1;
+	I2CReadLength		= 2;
+	I2CMasterBuffer[0] 	= MPU6050_ADRESS;
+	I2CMasterBuffer[1] 	= MPU6050_RA_ACCEL_YOUT_H;
+	I2CMasterBuffer[2] 	= MPU6050_ADRESS | RD_BIT;
+
+	state = I2CEngine();
+	if(state != I2C_OK) return 1;
+
+	tmp = (I2CSlaveBuffer[0] << 8) | I2CSlaveBuffer[1] ;
+
+	return tmp * 123; /* TBD */
+}
+
+float MPU6050_getAccel_z(){
+
+	uint16_t tmp;
+	uint8_t state;
+
+	I2CWriteLength 		= 1;
+	I2CReadLength		= 2;
+	I2CMasterBuffer[0] 	= MPU6050_ADRESS;
+	I2CMasterBuffer[1] 	= MPU6050_RA_ACCEL_ZOUT_H;
+	I2CMasterBuffer[2] 	= MPU6050_ADRESS | RD_BIT;
+
+	state = I2CEngine();
+	if(state != I2C_OK) return 1;
+
+	tmp = (I2CSlaveBuffer[0] << 8) | I2CSlaveBuffer[1] ;
+
+	return tmp * 123; /* TBD */
+}
---
+++ src/gyro_main.c
@@ -0,0 +1,75 @@
+
+
+#include "driver_config.h"
+#include "target_config.h"
+
+#include <math.h>
+#include <string.h>
+
+#include "gpio.h"
+#include "i2c.h"
+#include "uart.h"
+#include "type.h"
+
+#include "mpu6050.h"
+#include "ars.h"
+
+
+uint8_t gSysTick;
+
+void SysTick_Handler(void){
+	gSysTick++;
+}
+
+float get_Time_s(void){
+	float ticks = gSysTick;
+	gSysTick = 0;
+	return ticks / 1000; /* return in seconds */
+}
+
+int main (void){
+
+	struct Gyro1DKalman filter_roll;
+	float acc_tmp;
+	float dt;
+	float roll_angle;
+	uint8_t angle[10];
+	uint8_t crlf[] = { 0x0D, 0x0A }; // \n\r
+
+	/* Init Systick to 1ms */
+	SysTick_Config(0xFFFF); /* TBD */
+	/* Initialize GPIO (sets up clock) */
+	GPIOInit();
+	/* Set LED port pin to output */
+	GPIOSetDir( LED_PORT, LED_BIT, 1 );
+
+	UARTInit(UART_BAUD); // 115200;
+
+	if(I2CInit(I2CMASTER) == FALSE){
+	  while(1);	/* fatal error */
+	}
+
+
+	if(MPU6050_init()){
+		/*error*/
+	}
+
+	if(MPU6050_whoami()){
+		/*error*/
+		return 1;
+	}
+
+	/*kalman form http://tom.pycke.be/ */
+	init_Gyro1DKalman(&filter_roll, 0.0001, 0.0003, 0.69);
+
+	while(1){
+
+		dt = get_Time_s();
+		ars_predict(&filter_roll, MPU6050_getGyroRoll_rad() , dt);  // Kalman predict
+		acc_tmp = -(atan2(-MPU6050_getAccel_z(), MPU6050_getAccel_y())-(3.14159/2.0)); /* calculate accel angle */
+	    roll_angle = ars_update(&filter_roll, acc_tmp);        // Kalman update + result (angle)
+	    memcpy(angle, &roll_angle, sizeof(roll_angle));
+	    UARTSend( (uint8_t *) angle, 10);
+	    UARTSend( (uint8_t *) crlf, 2);
+	}
+}
